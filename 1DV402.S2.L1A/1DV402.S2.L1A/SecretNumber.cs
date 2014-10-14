c951250d007ad9f0efdea7d2fea59a97083ba69f
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-10-14 21:14:15 +0200

    Created basic class SecretNumber with members, started on method Initialize()

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    public class SecretNumber
+    {
+
+        private int _count;
+        private int _number;
+        public const int MaxNumberOfGuesses = 7;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SecretNumber()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes each new try by generating a random number between 1-100 and setting the _count field to zero
+        /// </summary>
+        public void Initialize()
+        {
+            Random random = new Random();
+            _count = 0;
+            _number = random.Next(1, 101);
+        }
+
+        public bool MakeGuess(int number)
+        {
+            throw new ArgumentException();
+        }
+
+    }
+}
